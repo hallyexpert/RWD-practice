fddc5d1a6539e1508a2775cedb16be629e08b48e
hallyexpert <s1094476@g.ncyu.edu.tw>
2022-09-10 16:11:48 +0800

    first commit

---
+++ js/Restaurant ver.2.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('body').on('click', 'a', function(event) {
+        event.preventDefault();
+      });  
+
+    // banner
+    $("a[href$='.svg']").addClass('fa-solid fa-utensils');
+    $("a[href$='.google']").addClass('fa-brands fa-square-google-plus');
+    $("a[href$='.twt']").addClass('fa-brands fa-square-twitter');
+    $("a[href$='.fb']").addClass('fa-brands fa-square-facebook');
+
+    // menu
+    $("a[href$='.menu']").addClass('fa-solid fa-bars');
+
+    $('.showbar').on('click',  function(e){
+        e.preventDefault();
+        $('body').toggleClass('menu-show');
+    });
+
+    $('.showbar').on('click',  function(e){
+        e.preventDefault();
+        $('body').toggleClass('open');
+    });
+
+    // cart
+    $("a[href$='.leaf']").addClass('fa-solid fa-leaf');
+    $("a[href$='.cart']").addClass('fa-solid fa-cart-shopping');
+    $("a[href$='.love']").addClass('fa-solid fa-heart');
+    $("a[href$='.large']").addClass('fa-solid fa-magnifying-glass');
+    $("a[href$='.dollar']").addClass('fa-solid fa-dollar-sign');
+});
